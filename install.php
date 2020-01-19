518cbd50c45365cccf1300937345e669fc3e2228
ZeeshanSultan <zeeshi7897@gmail.com>
2020-01-19 15:17:54 +0500

    Fixed automated setup

--- install.php
+++ install.php
@@ -3,21 +3,28 @@
 require_once 'config.php';
 
 $conn = new mysqli($hostname, $username, $password, $dbname);
-$query = '';
-$sqlScript = file('schema.sql');
-foreach ($sqlScript as $line) {
-	$startWith = substr(trim($line), 0, 2);
-	$endWith = substr(trim($line), -1, 1);
-	if (empty($line) || $startWith == '--' || $startWith == '/*' || $startWith == '//') {
-		continue;
-	}
-	$query = $query . $line;
-	if ($endWith == ';') {
-		mysqli_query($conn, $query) or die('Problem in executing the SQL query <b>' . $query . '</b>');
-		$query = '';
-	}
+
+if ($conn->connect_errno) {
+	printf("Connect failed: %s\n", $conn->connect_error);
+	die();
 }
 
-foreach($client_config as $name=>$client){
-	mysqli_query($conn, "INSERT INTO oauth_clients (client_id, client_secret, redirect_uri) VALUES ($client['id'], $client['pass'] , $client['redirect_url']");
+$handle = fopen("schema.sql", "r");
+while (($query = fgets($handle)) !== false) {
+	mysqli_query($conn, $query) or die('Problem in executing the SQL query <b>' . $query . '</b>');
 }
+fclose($handle);
+
+foreach ($client_config as $name => $client) {
+	$query = "INSERT INTO oauth_clients (client_id, client_secret, redirect_uri) VALUES ('" . $client['id'] . "', '" . $client['secret'] . "' , '" . $client['redirect_uri'] . "');";
+	mysqli_query($conn, $query) or die('Problem in executing the SQL query <b>' . $query . '</b>');
+}
+
+$query = "select client_id, client_secret from oauth_clients where 1";
+$result = mysqli_query($conn, $query);
+$table = "<div align='center'> Installation Successful. Share below credentials with integrator<br /><br /> <table border='1'><tbody><tr><th>Client Id</th><th>Client Secret</th></tr>";
+while ($row = $result->fetch_assoc()) {
+	$table .= "<tr><td>" . $row['client_id'] . "</td><td>" . $row['client_secret'] . "</td></tr>";
+}
+$table .= "</tbody></table></div>";
+echo $table;
