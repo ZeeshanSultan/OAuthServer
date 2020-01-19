24f2969ea9dc330599813d6395649775cb9318ce
ZeeshanSultan <zeeshi7897@gmail.com>
2020-01-19 12:55:16 +0500

    Fixed folder structure

--- server.php
+++ server.php
@@ -5,7 +5,7 @@ require_once 'config.php';
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-require_once 'oauth2-server-php/src/OAuth2/Autoloader.php';
+require_once 'OAuth2/Autoloader.php';
 OAuth2\Autoloader::register();
 $storage = new OAuth2\Storage\Pdo(array('dsn' => $dsn, 'username' => $username, 'password' => $password));
 $config = array('access_lifetime' => $access_token_lifetime, 'refresh_token_lifetime' => $refresh_token_lifetime);
